794f96c0ec1791979c1a70803f622be76ddb4fb3
Марков Олег <pogokoc888@rubeshi.com>
2023-09-30 18:32:05 +0300

    Almost updated into the POM

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.common.by import By
 import pytest
+from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    mp = MainPage(browser, link)
+    mp.open()
+    mp.go_to_login_page()
 
 if __name__ == "__main__":
     pytest.main(["-v", "--tb=line", "--language=en", "test_main_page.py"])
