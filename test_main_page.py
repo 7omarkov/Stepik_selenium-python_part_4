0c8e901c99af61ada43508bb0122682626ff8c24
Марков Олег <pogokoc888@rubeshi.com>
2023-10-01 22:33:03 +0300

    User should see login link test added

--- test_main_page.py
+++ test_main_page.py
@@ -4,12 +4,15 @@ from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    myvar = browser[1]
-    browser = browser[0]
-    print(myvar)
     mp = MainPage(browser, link)
     mp.open()
     mp.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 if __name__ == "__main__":
     pytest.main(["-v", "--tb=line", "--language=en", "test_main_page.py"])
