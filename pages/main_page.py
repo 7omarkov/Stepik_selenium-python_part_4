eb6a30d73ca7db85756c41ccf6d5ac97fc6a91d7
Марков Олег <pogokoc888@rubeshi.com>
2023-10-01 23:10:09 +0300

    Move to login page method updated

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,6 +3,7 @@ from pages.base_page import BasePage
 from .locators import MainPageLocators
 
 
+
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
