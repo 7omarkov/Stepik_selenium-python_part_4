92a8dfb3a2ae175f33001a6fbb9a74b63203dcf8
Oleg Markov <7omarkov@gmail.com>
2023-10-04 14:17:35 +0300

    Parametrization implemented for test_guest_can_add_product_to_basket update

--- conftest.py
+++ conftest.py
@@ -5,17 +5,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en", help="Input language")
+    pass
+    #parser.addoption('--language', action='store', default="en", help="Input language")
 
 @pytest.fixture()
 def browser(request):
 
-    my_language = request.config.getoption("--language")
+    '''my_language = request.config.getoption("--language")
     if not my_language:
-        raise pytest.UsageError("set --language!!")
+        raise pytest.UsageError("set --language!!")'''
 
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': my_language})
+    #options.add_experimental_option('prefs', {'intl.accept_languages': my_language})
     browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
